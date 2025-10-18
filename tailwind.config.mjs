b0f63d612dcb5fec62bee37655aa34e1e99637da
QuachNhutBao <83082808+QuachNhutBao@users.noreply.github.com>
2025-10-18 08:14:31 +0000

    Fix: Update config files to ES Modules syntax

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -15,4 +15,5 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
+export default config;
