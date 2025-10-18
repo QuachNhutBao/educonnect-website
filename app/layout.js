eb541862ae9fbbc5d2cebf6f8168281a439c5c56
QuachNhutBao <83082808+QuachNhutBao@users.noreply.github.com>
2025-10-18 12:52:36 +0000

    Hoàn tất cấu hình cơ bản và thêm nội dung đa ngôn ngữ

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,13 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-import { ThemeProvider } from "./theme-provider";
+import './globals.css'
+import { Inter } from 'next/font/google'
+import { ThemeProvider } from './theme-provider'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "EduConnect Services",
-  description:
-    "Connecting institutions with global talent, integrating cutting-edge EdTech, and fostering international partnerships.",
-};
+  title: 'EduConnect Services',
+  description: "Connecting institutions with global talent, integrating cutting-edge EdTech, and fostering international partnerships.",
+}
 
 export default function RootLayout({ children }) {
   return (
@@ -19,5 +18,5 @@ export default function RootLayout({ children }) {
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
